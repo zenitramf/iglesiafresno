c680a0b3d9fc54bc152d5369418f22f5f6c3a725
zenitramf <francisco@zenitram.dev>
2026-08-02 01:39:19 +0000

    starwind install

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
+import tailwindcss from "@tailwindcss/vite";
 // @ts-check
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  server: {
+	server: {
     allowedHosts: ["fresnovictory.ngrok.app"],
   },
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
